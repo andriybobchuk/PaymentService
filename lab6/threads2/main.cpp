2abc5ec503231edaf847ba2311dc3a4ea6dccc83
Andriy Bobchuk <andriybobchuk@gmail.com>
2022-04-21 00:40:09 +0200

    Lab6 Finished

--- lab6/threads2/main.cpp
+++ lab6/threads2/main.cpp
@@ -1,40 +1,119 @@
-// conditionVariable.cpp
-
-#include <iostream>
-#include <condition_variable>
-#include <mutex>
 #include <thread>
+#include <mutex>
+#include <iostream>
+#include <atomic>
+#include <future>
+#include <chrono>
+#include <vector>
 
-std::mutex m;
-std::condition_variable condVar;
-
-// 3. - Processes the data
-void doTheWork() {
-  std::cout << "Processing shared data." << std::endl;
+// For task 3:
+std::atomic<bool> ready (false);
+void count1m(int id) {
+    while (!ready) {  // wait until main() sets ready...
+        std::this_thread::yield();
+    }
+    for (int i=0; i<1000000; ++i) {}
+    std::cout << id << ' ';
 }
 
-// 1. - Waits until the data is ready for being processed
-void waitingForWork() {
-    std::cout << "Worker: Waiting for work." << std::endl;
-    std::unique_lock<std::mutex> lck(m);
-    condVar.wait(lck);
-    doTheWork();
-    std::cout << "Work done." << std::endl;
+// For task 4:
+std::mutex vectLock;
+std::vector<unsigned int> primeVec;
+void findPrimes(unsigned int start, unsigned int end) {
+    for (unsigned int x = start; x <= end; x++) {
+        if (x == 2) {
+            vectLock.lock();
+            primeVec.push_back(x);
+            vectLock.unlock();
+        }
+        for (unsigned int y = 2; y < x; y++) {
+            if ((x % y) == 0) {
+                break;
+            }
+            else if ((y + 1) == x) {
+                vectLock.lock();
+                primeVec.push_back(x);
+                vectLock.unlock();
+            }
+        }
+    }
 }
 
-// 2. - Notifies that the data is ready for being processed
-void setDataReady() {
-    std::cout << "Sender: Data is ready."  << std::endl;
-    condVar.notify_one();
+void findPrimesWithThreads(unsigned int start, unsigned int end,
+                           unsigned int numThreads) {
+    std::vector<std::jthread> threadVect;
+    unsigned int threadSpread = end / numThreads;
+    unsigned int newEnd = start + threadSpread - 1;
+
+    for (unsigned int x = 0; x < numThreads; x++) {
+        threadVect.emplace_back(findPrimes, start, newEnd);
+        start += threadSpread;
+        newEnd += threadSpread;
+    }
 }
 
 int main() {
 
-    // is locked until setDataReady() unlocks it
-  std::thread t1(waitingForWork);
-    // unlocks waitingForWork() so doTheWork() is started
-  std::thread t2(setDataReady);
+    // Task 1
+    // Create a lambda function which increments an integer by one.
+    // Create a pool with 50 threads and let all of them execute this function.
+    // Check the consistency in results, explain it.
+    auto add_1 = [](std::atomic<int> &i) { i += 1; };
+    int i = 1;
+    std::cout << i << '\n';
+    std::thread threads[49];
+    for (auto &e: threads) {
+        e = std::thread(add_1, std::ref(i));
+    }
+    for (auto &th: threads) {
+        th.join();
+    }
+    std::cout << '\n' << i << '\n'; // prints 3
 
-  t1.join();
-  t2.join();
+    // Task 2
+    // Create a new version of the task 1 using mutex or atomic variables to avoid races.
+    // How did the output change?
+    auto add_1 = [](std::atomic<int> &i) { i += 1; };
+    std::atomic<int> i = 1;
+    std::cout << i << '\n';
+    std::thread threads[49];
+    for (auto &e: threads) {
+        e = std::thread(add_1, std::ref(i));
+    }
+    for (auto &th: threads) {
+        th.join();
+    }
+    std::cout << '\n' << i << '\n'; // prints 3
+
+    // Task 3
+    // Create a function which will simply iterate through 1mln times.
+    // In the pool of 10 threads execute this function but all threads should wait and start at
+    // the same time, when the whole pool is defined.
+    std::thread threads[10];
+    std::cout << "race of 10 threads that count to 1 million:\n";
+    for (int i=0; i<10; ++i) threads[i]=std::thread(count1m,i);
+    ready = true; // go!
+    for (auto& th : threads) th.join();
+
+    // Task 4
+    // Create a function or functions that together fill a vector with prime numbers from a given range
+    // (e.g. 1-100000). Do it such that you can specify the number of threads you want to use,
+    // measure time, and compare it.
+    int startTime = clock();
+    findPrimesWithThreads(1, 10000, 1);
+    int endTime = clock();
+
+    //for (auto &n : primeVec) std::cout << n << std::endl;
+    std::cout << "Execution time (one thread): "
+              << (endTime - startTime) / double(CLOCKS_PER_SEC) << std::endl;
+
+    primeVec.clear();
+    startTime = clock();
+    findPrimesWithThreads(1, 10000, 3);
+    endTime = clock();
+
+    std::cout << "Execution time (more threads): "
+              << (endTime - startTime) / double(CLOCKS_PER_SEC) << std::endl;
+
 }
+
